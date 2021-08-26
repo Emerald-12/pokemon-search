bf1169cedcdd3ed7c9545cdae4859e01935b2e0d
Tomkhcoding <tom.kodehode@gmail.com>
2021-08-26 17:31:06 +0200

    a

--- src/components/Output.js
+++ src/components/Output.js
@@ -9,7 +9,7 @@ function Output(props) {
                         ?{ background: `linear-gradient(135deg, ${props.cardColour[0]} 20%, ${props.cardColour[1]} 80%)`}
                         :{ backgroundColor: 'silver' }}>
 
-                <div className={styles.PokeText}>
+                <div className={styles.pokeText}>
                     <h1 className={styles.pokeCardTitle}>{props.pokeInfo.name}</h1>
                     <h2 className={styles.dexEntry}>{props.pokeEntry}</h2>
                 </div>
@@ -34,7 +34,8 @@ function Output(props) {
                 <div style={{display:'flex',
                 flexDirection:'row'}}>
                     <div style={{display:'flex',
-                    flexDirection:'column', width:'50%'}}>
+                    flexDirection:'column', width:'50%',
+                    margin:'5px'}}>
                         <p>HP: {props.pokeInfo.stats[0].base_stat}</p>
                         <p>Attack: {props.pokeInfo.stats[1].base_stat}</p>
                         <p>Defense: {props.pokeInfo.stats[2].base_stat}</p>
@@ -43,7 +44,7 @@ function Output(props) {
                     <div style={{display:'flex',
                     flexDirection:'column',
                     width: '50%',
-                    margin:'auto'}}>
+                    margin:'5px'}}>
                         <p>S. Attack: {props.pokeInfo.stats[3].base_stat}</p>
                         <p>S. Defence: {props.pokeInfo.stats[4].base_stat}</p>
                         <p>Speed: {props.pokeInfo.stats[5].base_stat}</p>
